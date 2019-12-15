9385a022f33736aab1159d065afa2b603e75f38a
Emilia Vestlund <emiliavestlund@hotmail.com>
2019-12-15 22:57:34 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ build/precache-manifest.f7b8969de14c964e376317103715841b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f036392a0ae96db1a93300d8884a10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e4ccc2678f0afc6542",
+    "url": "/static/css/main.1de3576f.chunk.css"
+  },
+  {
+    "revision": "b7968d99eb5b4ddd97a5",
+    "url": "/static/js/2.c4c7df03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c4c7df03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01e4ccc2678f0afc6542",
+    "url": "/static/js/main.e6120223.chunk.js"
+  },
+  {
+    "revision": "633ad3278563d8138e7d",
+    "url": "/static/js/runtime-main.d725bfa7.js"
+  },
+  {
+    "revision": "43072f04300e3dd36cf832b516c55f56",
+    "url": "/static/media/app3.43072f04.png"
+  },
+  {
+    "revision": "bc330f661623fe447ed00020f2590c5f",
+    "url": "/static/media/calender.bc330f66.jpg"
+  },
+  {
+    "revision": "a1eb25fb44819b15d1e1c7b9449d5d40",
+    "url": "/static/media/digitaldie1.a1eb25fb.png"
+  },
+  {
+    "revision": "01b9b5f6b8b4cebd4eb9ba4503fc9b83",
+    "url": "/static/media/frame3.01b9b5f6.png"
+  },
+  {
+    "revision": "ae0d0b1ab5e6d9b82d372d6ba8fa0b57",
+    "url": "/static/media/laddaned.ae0d0b1a.jpeg"
+  },
+  {
+    "revision": "5919fcbb9c4054854d2cc0246478aa1a",
+    "url": "/static/media/mobilapp.5919fcbb.png"
+  },
+  {
+    "revision": "2586900fd146cdf380005bbb37cf90cd",
+    "url": "/static/media/nav.2586900f.png"
+  }
+]);
